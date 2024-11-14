ca073775428f50287453f2166fc46d5b8df5e03c
MelihKrts <melih.karatas1281@gmail.com>
2024-11-14 11:55:52 +0300

    pwa regulation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
+  reactStrictMode: false,
 };
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
