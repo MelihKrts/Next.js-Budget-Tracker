34afe78f06fa8d58f5f7ed46aca2cb2a0169729b
MelihKrts <melih.karatas1281@gmail.com>
2024-11-14 13:16:20 +0300

    pwa mobile support work

--- app/layout.js
+++ app/layout.js
@@ -17,17 +17,30 @@ export const metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
-    apple: [{ url: "/ios/180.png", sizes: "180x180", type: "image/png" }],
+    apple: [
+      { url: "/ios/180.png", sizes: "180x180", type: "image/png" },
+      { url: "/ios/152.png", sizes: "152x152", type: "image/png" },
+      { url: "/ios/144.png", sizes: "144x144", type: "image/png" },
+      { url: "/ios/120.png", sizes: "120x120", type: "image/png" },
+    ],
+    shortcut: ["/favicon.ico"],
   },
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
     title: "Budget Tracking",
+    startupImage: [
+      {
+        url: "/ios/1024.png",
+        sizes: "1024x1024",
+      },
+    ],
   },
   viewport: {
     width: "device-width",
     initialScale: 1,
     maximumScale: 1,
+    userScalable: false,
   },
   themeColor: "#cbdfbd",
   formatDetection: {
@@ -42,6 +55,10 @@ export default function RootLayout({ children }) {
         <meta name="application-name" content="Budget Tracking" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#cbdfbd" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Budget Tracking" />
+        <link rel="apple-touch-startup-image" href="/ios/1024.png" />
       </head>
       <body className={`${exo.className}  bg-[#cbdfbd]`}>
         <Header />
